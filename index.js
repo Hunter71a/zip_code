af9e818fd15a6504ea685350c749c861ae441650
Carl Olson <colson2275@gmail.com>
2023-04-25 08:58:47 -0400

    correct casing of header items

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const request = require('request');
 
 console.log("Running");
 
+//list of tested zip codes
 const testedZip_Codes = ['32517', '73101', '90608', '10210', '25436', '48121', '71000', '18310', '73101', '48121', '07477', '56632', '20122', '25000',
   '6100', '15800', '75832','21139','33195','23905','88227','48046','56632','76057','49041','81100','46519','50059','97512','90608','31199','25436','10210',
     '82602','84551','33195','23905','75085','21139','36996','94142','75832','88227','21139'];
 
-const ZIP_CODES = ['27611'];
+// Zip Codes to test
+const ZIP_CODES = ['27611','13311', '2000','5107', '38197','12345','76101','57491','04122','99511',  '80912','43234','25781','65328','20100','11011','79409','07689','77383' ,'62304','77710','28028', '89254','00937','93700','11123','77556','45451' ,'42820','52546','46801','08601','22785', '90927','30015', '91954', '72203','92467','92581','30015' ];
 const numberOfZips = ZIP_CODES.length;
 let counter = 0;
 
@@ -21,10 +23,10 @@ const workbook = xlsx.utils.book_new();
 //const sheet = xlsx.utils.json_to_sheet([{ zip_code: "", state: "", valid_zip: "", city: "", country: "" }]);
 
 // create sheet and add headers
-const sheet = xlsx.utils.aoa_to_sheet([["Zip Code", "State", "Valid Zip?", "City", "Country"]]);
+const sheet = xlsx.utils.aoa_to_sheet([["zipCode", "city", "state", "Valid Zip?", "Country"]]);
 
 
-function capitalizeFirstLetter(string) {
+function capitalizeFirstLetter (string) {
   string = string.toLowerCase();
   let words = string.split(" ");
   string = words.map((word) => {
@@ -51,7 +53,7 @@ for (const zipCode of ZIP_CODES) {
         const country = data.country ? data.country : "none";
         const error = data.error ? "Bad" : "Good";
 
-        xlsx.utils.sheet_add_aoa(sheet, [[zipCode, state, error, city, country]], { origin: -1 });
+        xlsx.utils.sheet_add_aoa(sheet, [[zipCode, city, state, error, country]], { origin: -1 });
         counter += 1;
         console.log(counter);
       }
